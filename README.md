6f58d60863dc028d44f943e0fb18c7ea81146ba4
cdfortes <cdfortes@gmail.com>
2021-06-21 15:23:51 -0100

    refactoring and change colors

---
+++ README.md
@@ -0,0 +1,15 @@
+# TO-DO LIST
+
+Uma aplicação simples para adição de tarefas em uma lista, podendo pesquisar e remover tarefas
+
+[Demo](https://todo-list-cdfortes.netlify.app/)
+
+##
+
+<img src="screenshot-127.0.0.1_5500-2021.06.21-15_19_58.png" alt>
+
+## Tecnologias
+
+- HTML
+- CSS (Bootstrap)
+- JS
--- app.js
+++ app.js
@@ -2,53 +2,82 @@ const formAddTodos = document.querySelector('.form-add-todo')
 const formSearch = document.querySelector('.form-search input')
 const todoContainer = document.querySelector('.todos-container')
 
+const setIdToIcon = () => {
+  todoContainer.querySelectorAll('.delete').forEach((element, id) => {
+    element.setAttribute('id', id)
+  })
+}
+
 const addTodo = (todo) => {
   todoContainer.innerHTML += `
   <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${todo}</span>
-      <i class="far fa-trash-alt delete"></i>
+      <i id="" class="far fa-trash-alt delete"></i>
   </li>
   `
+  setIdToIcon()
 }
 
 const resetInput = (target) => target.reset()
+
+const deleteTodo = (target) => {
+  const targetClassList = target.classList
+  const isClassDelete = Array.from(targetClassList).includes('delete')
+
+  if (isClassDelete) {
+    const id = target.getAttribute('id')
+    const todoToRemove = todoContainer.children[Number(id)]
+
+    todoToRemove.remove()
+    setIdToIcon()
+  }
+}
+
+const removeClassFromElement = (element, className) =>
+  element.classList.remove(className)
+
+const addClassToElement = (element, className) =>
+  element.classList.add(className)
+
+const searchTodo = (todoList, termToSearch, hasHiddenClass) => {
+  const dflexClass = 'd-flex'
+  const hiddenClass = 'hidden'
+
+  todoList
+    .filter((todo) =>
+      !hasHiddenClass
+        ? !todo.textContent.toLowerCase().includes(termToSearch)
+        : todo.textContent.toLowerCase().includes(termToSearch)
+    )
+    .forEach((todo) => {
+      removeClassFromElement(todo, !hasHiddenClass ? dflexClass : hiddenClass)
+      addClassToElement(todo, !hasHiddenClass ? hiddenClass : dflexClass)
+    })
+}
 
 formAddTodos.addEventListener('submit', (event) => {
   event.preventDefault()
   const target = event.target
   const todo = target.add.value.trim()
-
-  addTodo(todo)
-  resetInput(target)
+  const hasTextInTheInput = todo.length > 0
+  
+  if (hasTextInTheInput) {
+    addTodo(todo)
+    resetInput(target)
+  }
 })
 
 todoContainer.addEventListener('click', (event) => {
   const target = event.target
-  console.log(target)
-  const targetClassList = target.classList
-  const isClassDelete = Array.from(targetClassList).includes('delete')
 
-  if (isClassDelete) {
-    target.parentElement.remove()
-  }
+  deleteTodo(target)
 })
 
 formSearch.addEventListener('input', (event) => {
   event.preventDefault()
   const termToSearch = event.target.value.toLowerCase().trim()
-  const todoLis = todoContainer.querySelectorAll('li')
+  const todoLis = todoContainer.children
 
-  Array.from(todoLis)
-    .filter((todo) => !todo.textContent.toLowerCase().includes(termToSearch))
-    .forEach((todo) => {
-      todo.classList.remove('d-flex')
-      todo.classList.add('hidden')
-    })
-
-  Array.from(todoLis)
-    .filter((todo) => todo.textContent.toLowerCase().includes(termToSearch))
-    .forEach((todo) => {
-      todo.classList.remove('hidden')
-      todo.classList.add('d-flex')
-    })
+  searchTodo(Array.from(todoLis), termToSearch, false)
+  searchTodo(Array.from(todoLis), termToSearch, true)
 })
